07d3c539cfd170a885825394d22467d5f87c034f
louca75 <louca.mendy@gmail.com>
2018-01-11 15:48:37 +0100

    navigation works

---
+++ src/pages/users/users.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the UsersPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-users',
+  templateUrl: 'users.html',
+})
+export class UsersPage {
+
+
+
+}
+
+}
